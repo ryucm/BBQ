3f4398e497df0a0e766b8ad4a908346c718e9922
Eden <tridge@ip-192-168-0-202.ap-northeast-2.compute.internal>
2022-07-27 10:47:46 +0900

    Add pagination

--- src/mahagrid/index.js
+++ src/mahagrid/index.js
@@ -1,21 +1,40 @@
 
 import request from 'request-promise';
 import * as cheerio from 'cheerio';
+
+
 import {
   Producer,
   Consumer,
 } from '../../lib/jobs';
 import { Crawler } from '../../lib/crawler'
+
+
 const baseUrl = 'http://mahagrid.com';
 class MahagridProducer extends Producer {
   async produce() {
+    let pageNum = this.constructor.pageStart;
+    let pageNation = true;
+    while(pageNation) {
+      const pageUrl = `/product/list.html?cate_no=24&page=${pageNum}`;
+      const url = baseUrl+pageUrl;
+      pageNation = await this.fetch(url);
+      pageNum += this.constructor.pageStep;
+      if (this.constructor.pageMax >= 0
+        && pageNum > this.constructor.pageMax) {
+        break;
+      }
+    }
+  }
+
+  async fetch(url) {
     let response;
-    const pageUrl = `${baseUrl}/product/list.html?cate_no=24&page={0}`;
     try {
-      response = await request(pageUrl);
-      console.log(`producing : ${pageUrl}`);
+      response = await request(url);
+      console.log(`producing : ${url}`);
     } catch (e) {
       console.log(e);
+      return false;
     }
     const $ = cheerio.load(response);
     $('.mun-prd-thumb > a').each((_i,el) => {
@@ -23,13 +42,14 @@ class MahagridProducer extends Producer {
       const url = baseUrl + parseUrl;
       this.push(url);
     })
+    return true;
   }
 }
 
 class MahagridConsumer extends Consumer {
   async consume(url) {
     console.log(`consuming ${url}`);
-    this.meta.pusher.push();
+    this.meta.pusher.push(url);
   }
 }
 
